b2f5e10715719ff698dc002cb534c490e7846869
ousttrue <ousttrue@gmail.com>
2015-11-01 01:08:22 +0900

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,11 @@ var gulp = require("gulp");
 // sass
 //
 var sass = require("gulp-sass");
-
+var autoprefixer = require("gulp-autoprefixer");
+ 
 gulp.task("sass", function() {
     gulp.src("src/sass/**/*scss")
         .pipe(sass())
+        .pipe(autoprefixer())
         .pipe(gulp.dest("out/css"));
 });
