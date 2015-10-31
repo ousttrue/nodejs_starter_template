c9753653a9a9ed3a578f6f2de9c84a2011897817
ousttrue <ousttrue@gmail.com>
2015-11-01 01:05:47 +0900

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
 var gulp = require("gulp");
+
+//
+// sass
+//
+var sass = require("gulp-sass");
+
+gulp.task("sass", function() {
+    gulp.src("src/sass/**/*scss")
+        .pipe(sass())
+        .pipe(gulp.dest("out/css"));
+});
