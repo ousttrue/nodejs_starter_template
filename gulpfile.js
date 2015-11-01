731a41c7a9dbc8043e09993d885d126b66ded069
ousttrue <ousttrue@gmail.com>
2015-11-01 14:09:31 +0900

    bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -51,18 +51,28 @@ gulp.task("html", function(){
 //
 // sass
 //
-var sass = require("gulp-sass");
-var autoprefixer = require("gulp-autoprefixer");
-var frontnote = require("gulp-frontnote");
-gulp.task("sass", function() {
-    gulp.src("src/sass/**/*scss")
+gulp.task('css', function() {
+    return gulp.src(config.sassPath + '/**/*scss')
         .pipe($.plumber({errorHandler: $.notify.onError('<%= error.message %>')}))
-        .pipe(frontnote({
+        .pipe($.frontnote({
             css: '../out/css/style.css'
         }))
-        .pipe(sass())
-        .pipe(autoprefixer())
-        .pipe(gulp.dest("out/css"))
+        .pipe($.sass({
+            //outputStyle: 'compressed',
+            includePaths: [
+                './src/sass',
+                config.bowerDir + '/bootstrap-sass/assets/stylesheets',
+                config.bowerDir + '/font-awesome/scss',
+            ]
+        })
+            /*
+            .on("error", $.notify.onError(function (error) {
+                return "Error: " + error.message;
+            }))
+            */
+        )
+        .pipe($.autoprefixer())
+        .pipe(gulp.dest('./out/css'))
         .pipe(browser.reload({stream:true}))
         ;
 });
@@ -70,7 +80,6 @@ gulp.task("sass", function() {
 //
 // js
 //
-var uglify = require("gulp-uglify");
 gulp.task("js", function() {
     gulp.src(["src/js/**/*.js","!src/js/min/**/*.js"])
         .pipe($.plumber({errorHandler: $.notify.onError('<%= error.message %>')}))
@@ -79,7 +88,7 @@ gulp.task("js", function() {
         .on('error', function (error) {
             console.error(String(error));
         })        
-        .pipe(uglify())
+        .pipe($.uglify())
         .pipe(gulp.dest("out/js/min"))
         .pipe(browser.reload({stream:true}))
         ;
@@ -88,8 +97,10 @@ gulp.task("js", function() {
 //
 // watch
 //
-gulp.task("default", ['server'], function() {
+gulp.task("watch", ['server'], function() {
     gulp.watch(["src/js/**/*.js","!src/js/min/**/*.js"],["js"]);
-    gulp.watch("src/sass/**/*.scss",["sass"]);
+    gulp.watch("src/sass/**/*.scss",["css"]);
     gulp.watch("src/*.html",["html"]);
 });
+
+gulp.task('default', ['bower', 'icons', 'css']);
